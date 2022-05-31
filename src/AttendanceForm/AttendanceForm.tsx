ef7c8d2a910c357f2ab680ebf06e6d0c02017726
wolfeius <ajcastany@gmail.com>
2022-05-31 21:16:44 +0100

    ADD: Centered table

--- src/AttendanceForm/AttendanceForm.tsx
+++ src/AttendanceForm/AttendanceForm.tsx
@@ -6,25 +6,28 @@ function AttendanceForm() {
     console.log(JSON.stringify(MOCK_DATA, null, ''));
     return (
         <div>
-            <h2>Attendance Form</h2>
-            <table className="table">
-                <thead>
-                    <tr>
-                        <th scope="col">ID</th>
-                        <th scope="col">Name</th>
-                        <th scope="col">Department</th> 
-                        <th scope="col">Meeting Room</th>
-                        <th scope="col">Time In</th>
-                        <th scope="col">Time Out</th>
-                        <th scope="col">Tag Issued</th>
-                        <th scope="col">Tag Returned?</th>
-                    </tr>
-                </thead>
-                <tbody>
-                        <AttendanceList staffs={MOCK_DATA}/>        
-                </tbody>
-            </table>
-            
+            <div className="blue-strip">
+                <h2 className="display-4 text-center"><strong>Attendance Form</strong></h2>
+            </div>
+                <div className="container">
+                <table className="table table-striped table-hover table-responsive">
+                    <thead className="table-dark">
+                        <tr>
+                            <th scope="col">ID</th>
+                            <th scope="col">Name</th>
+                            <th scope="col">Department</th> 
+                            <th scope="col">Meeting Room</th>
+                            <th scope="col">Time In</th>
+                            <th scope="col">Time Out</th>
+                            <th scope="col">Tag Issued</th>
+                            <th scope="col">Tag Returned?</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                            <AttendanceList staffs={MOCK_DATA}/>        
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 }
