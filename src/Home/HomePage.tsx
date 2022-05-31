ecdaee08c9f69cb3e848241eda902810f2c40262
wolfeius <ajcastany@gmail.com>
2022-05-31 13:53:55 +0100

    FIX: removed unussed imports from HomePage.tsx

--- src/Home/HomePage.tsx
+++ src/Home/HomePage.tsx
@@ -1,7 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import StartHere from "../Start/StartHere";
-
+import { Link } from "react-router-dom";
 
 function HomePage() {
     return (
