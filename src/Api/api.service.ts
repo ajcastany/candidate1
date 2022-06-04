b4e7a95f783b0b34f6b3fa8b9fbf726ca6e49fdd
wolfeius <ajcastany@gmail.com>
2022-06-04 13:53:18 +0100

    ADD: api.service and http-commons

---
+++ src/Api/api.service.ts
@@ -0,0 +1,19 @@
+import http from './http-commons';
+
+class ApiService {
+    getAllStaff() {return http.get('/api/staff/all');}
+    getStaff(id:number) {return http.get('/api/staff/${id}');}
+    getDay(day:string) {return http.get('/api/daily_form/${day}');}
+    getAllDays() {return http.get('/api/daily_form/all_days');}
+    addRoom(data:string) {return http.post('/api/daily_form/room', data);}
+    addTimeInOut(data:string) {return http.post('/api/daily_form/time', data);}
+    addTag(data:string) {return http.post('/api/daily_form/tag', data);}
+    tagRet(data:string) {return http.post('api/daily_form/tag_ret', data);}
+}
+
+
+
+
+
+
+
