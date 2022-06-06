086f72e2c08c93be807f07c3afae9e1dd93d677a
wolfeius <ajcastany@gmail.com>
2022-06-06 18:39:37 +0100

    DEL: Modal close button
    not working

--- src/AttendanceForm/TagModal.tsx
+++ src/AttendanceForm/TagModal.tsx
@@ -24,12 +24,12 @@ function TagModal(props: TagModelProps) {
     return (
         <Modal
             show={props.showModal}
-            //onHide={props.closeModal()}
             backdrop="static"
             keyboard={false}
             cancel={() => props.closeModal()}
+
             >
-            <Modal.Header closeButton>
+            <Modal.Header>
                 <Modal.Title>Issue Tag</Modal.Title>
             </Modal.Header>
             <Modal.Body>
