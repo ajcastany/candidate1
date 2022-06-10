22b1dfa6f8fd887b26a30b32a0a35a2432418be0
wolfeius <ajcastany@gmail.com>
2022-06-10 14:56:02 +0100

    ADD: yes tag ret function

--- src/AttendanceForm/TagReturnedModal.tsx
+++ src/AttendanceForm/TagReturnedModal.tsx
@@ -12,7 +12,24 @@ interface TagReturnedProps {
 
 function TagReturnedModal(props: TagReturnedProps) {
     function YesTagReturned() {
+        const data:IDailyForm = {
+            day: new Date(0),
+            id: props.id,
+            name: 0,
+            room: "",
+            time_in: new Date(0).toISOString(),
+            time_out: new Date(0).toISOString(),
+            tag: props.tag,
+            tag_ret: true
+        }
 
+        var dataTXT = JSON.stringify(data);
+        var dataJSON = JSON.parse(dataTXT);
+        StaffApiService.addTagRetJSON(dataJSON).then(
+            res => {
+                props.closeModal();
+            }
+        )
     }
 
     function NoTagReturned() {
